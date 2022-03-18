281db912969f5844c8a4da36e80a09ae9d6a61ba
SaiKarthikYamavaram <ysaikarthiksep01@gmail.com>
2022-03-18 14:51:36 +0530

    added dependencies to cmake

--- android/cpp-adapter.cpp
+++ android/cpp-adapter.cpp
@@ -1,8 +1,7 @@
 #include <jni.h>
 #include "react-native-sequel.h"
 
-extern "C"
-JNIEXPORT jint JNICALL
+extern "C" JNIEXPORT jint  extern "C" JNICALL
 Java_com_reactnativesequel_SequelModule_nativeMultiply(JNIEnv *env, jclass type, jint a, jint b) {
     return example::multiply(a, b);
 }
